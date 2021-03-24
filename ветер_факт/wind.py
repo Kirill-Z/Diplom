ed8a9eb84fff642b474f8aa2f40d490dde885c9f
Kirill-Z <zyev.99@mail.ru>
2021-03-24 11:25:39 +0700

    Added calculation of wind speed by vectors

--- ветер_факт/wind.py
+++ ветер_факт/wind.py
@@ -12,22 +12,43 @@ with open('/home/ubuntu/Desktop/Diplom/DATA_GFS/2015010100_00') as r_file:
 
     for i in range(0, 70):
         file_list[i] = file_list[i][0].split()
-    #print(file_list[0][1])
-    print(len(file_list[1]))
-    correctData = []
 
-    for i in range(len(file_list)):
-        correctData.append([])
-
+    correctDataUGRD = []
+    correctDataVGRD = []
     numRows = 0
     for i in range(len(file_list)):
         if file_list[i][0] == '04':
-            correctData[numRows].append(file_list[i][0])
-            correctData[numRows].append(file_list[i][1])
+            correctDataUGRD.append([])
+            correctDataUGRD[numRows].append(file_list[i][0])
+            correctDataUGRD[numRows].append(file_list[i][1])
             numRows += 1
             for j in range(0, len(file_list[i])):
                 if 1442 < j < 1532:
-                    print(numRows)
-                    correctData[numRows-1].append(file_list[i][j])
+                    correctDataUGRD[numRows-1].append(file_list[i][j])
 
-    print(correctData[0])
+    numRows = 0
+    for i in range(len(file_list)):
+        if file_list[i][0] == '05':
+            correctDataVGRD.append([])
+            correctDataVGRD[numRows].append(file_list[i][0])
+            correctDataVGRD[numRows].append(file_list[i][1])
+            numRows += 1
+            for j in range(0, len(file_list[i])):
+                if 1442 < j < 1532:
+                    correctDataVGRD[numRows-1].append(file_list[i][j])
+
+    speedWind = []
+    numRows = 0
+    for i in range(0, len(correctDataVGRD)):
+        speedWind.append([])
+        speedWind[numRows].append(correctDataVGRD[i][1])
+        numRows += 1
+        for j in range(2, len(correctDataVGRD[i])):
+            speedWind[numRows-1].append(((float(correctDataUGRD[i][j])**2)+(float(correctDataVGRD[i][j])**2))**(1/2))
+
+
+    print(len(speedWind))
+    #for i in range(0, len(speedWind)):
+    print(speedWind[7])
+
+
