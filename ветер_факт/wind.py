db29e4e6895ffd71c236ef0c88bd8835599c991e
Kirill-Z <zyev.99@mail.ru>
2021-04-06 16:51:02 +0700

    Adding the ability to process all the necessary files in a directory

--- ветер_факт/wind.py
+++ ветер_факт/wind.py
@@ -1,54 +1,53 @@
 import pandas as pd
+import os
+import re
 
-with open('/home/ubuntu/Desktop/Diplom/DATA_GFS/2015010100_00') as r_file:
-    file_reader = pd.read_csv(r_file, sep='/s', skiprows=1, header=None, engine='python')
-    count = 0
-    occurNum = 2
-    pattern = '/-?/d{1,}'
-
-
-    file_reader.dropna(inplace=True)
-    file_list = file_reader.values.tolist()
-
-    for i in range(0, 70):
-        file_list[i] = file_list[i][0].split()
-
-    correctDataUGRD = []
-    correctDataVGRD = []
+def gettingNeedData(windDirection):
     numRows = 0
-    for i in range(len(file_list)):
-        if file_list[i][0] == '04':
-            correctDataUGRD.append([])
-            correctDataUGRD[numRows].append(file_list[i][0])
-            correctDataUGRD[numRows].append(file_list[i][1])
+    if windDirection == '04':
+        correctData = 'correctDataUGRD_' + file
+        correctData = []
+    elif windDirection == '05':
+        correctData = 'correctDataVGRD_' + file
+        correctData = []
+
+    for i in range(len(data_from_file)):
+        if data_from_file[i][0] == windDirection:
+            correctData.append([])
+            correctData[numRows].append(data_from_file[i][0])
+            correctData[numRows].append(data_from_file[i][1])
             numRows += 1
-            for j in range(0, len(file_list[i])):
+            for j in range(0, len(data_from_file[i])):
                 if 1442 < j < 1532:
-                    correctDataUGRD[numRows-1].append(file_list[i][j])
-
-    numRows = 0
-    for i in range(len(file_list)):
-        if file_list[i][0] == '05':
-            correctDataVGRD.append([])
-            correctDataVGRD[numRows].append(file_list[i][0])
-            correctDataVGRD[numRows].append(file_list[i][1])
-            numRows += 1
-            for j in range(0, len(file_list[i])):
-                if 1442 < j < 1532:
-                    correctDataVGRD[numRows-1].append(file_list[i][j])
-
-    speedWind = []
-    numRows = 0
-    for i in range(0, len(correctDataVGRD)):
-        speedWind.append([])
-        speedWind[numRows].append(correctDataVGRD[i][1])
-        numRows += 1
-        for j in range(2, len(correctDataVGRD[i])):
-            speedWind[numRows-1].append(((float(correctDataUGRD[i][j])**2)+(float(correctDataVGRD[i][j])**2))**(1/2))
+                    correctData[numRows - 1].append(data_from_file[i][j])
+    return correctData
 
 
-    print(len(speedWind))
-    #for i in range(0, len(speedWind)):
-    print(speedWind[7])
+PATH = "/media/kirill/e61c7b4d-3c04-47cc-aabb-23d698198ced/home/kirill/Downloads/EarlyData/Project/DATA_GFS/"
+
+for file in os.listdir(PATH):
+    if re.match('\d{10}_\d{2}.csv', file):
+        currentFile = PATH + file
+        file_reader = pd.read_csv(currentFile, sep='/s', skiprows=1, header=None, engine='python')
+        file_reader.dropna(inplace=True)
+        data_from_file = file_reader.values.tolist()  # Getting a list of data from a file
+
+        for i in range(0, 70):  # 70 lines in each gfc file
+            data_from_file[i] = data_from_file[i][0].split()
+
+
+        correctDataUGRD = gettingNeedData('04')
+        correctDataVGRD = gettingNeedData('05')
+
+        speedWind = []
+        numRowsForSpeed = 0
+        for i in range(0, len(correctDataVGRD)):
+            speedWind.append([])
+            speedWind[numRowsForSpeed].append(correctDataVGRD[i][1])
+            numRowsForSpeed += 1
+            for j in range(2, len(correctDataVGRD[i])):
+                speedWind[numRowsForSpeed-1].append(((float(correctDataUGRD[i][j])**2)+(float(correctDataVGRD[i][j])**2))**(1/2))
+
+        print(len(speedWind))
 
 
