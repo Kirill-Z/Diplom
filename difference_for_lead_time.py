05d019073a0d967d5d7703b45a1e62ef366590e7
Kirill-Z <zyev.99@mail.ru>
2021-05-15 12:00:42 +0700

    Changed the way of displaying the average difference

--- difference_for_lead_time.py
+++ difference_for_lead_time.py
@@ -16,7 +16,8 @@ def calc_diff(lead_time, diff_num_lead_time):
     if speed_wind_predictive_true[i][j][0][11:13] == lead_time:
         if i + j * 8 < (len(speed_wind_practical) - 1):
             if speed_wind_predictive_true[i][j][3] == int(speed_wind_practical[i + j * 8][3]):
-                if float(speed_wind_predictive_true[i][j][5]) >= 9999 or math.isnan(float(speed_wind_practical[i + j * 8][5])):
+                if float(speed_wind_predictive_true[i][j][5]) >= 9999 or math.isnan(
+                        float(speed_wind_practical[i + j * 8][5])):
                     pass
                 else:
                     diff_num_lead_time.append(
@@ -34,7 +35,7 @@ def calc_the_average_diff_for_the_lead_time(diff_lead_time):
 
 
 tmp = input(
-    'Press 1 if you want to calculate values or press 2 if you want to select ready-made speed data from a file: ')
+    'Press 1 if you want to calculate values')
 if tmp == "1":
     value = input(
         "Forecast data: If you need to calculate a point, press 1, if you need to calculate an area, press 2: ")
@@ -49,16 +50,13 @@ speed_wind_predictive_true = []
 speed_wind_predictive = file_reader.values.tolist()
 
 speed_wind_practical = practical_wind_forecast.main('3')
-#print(speed_wind_predictive)
-#print(speed_wind_practical)
+# print(speed_wind_predictive)
+# print(speed_wind_practical)
 lead_time = ('00', '03', '06', '09', '12', '15', '18', '21', '24', '27', '30', '33', '36', '39', '42', '45', '48', '51',
              '54', '57', '60', '63', '66', '69', '72', '75', '78')
 
 for i in lead_time:
     speed_wind_predictive_true.append(write_in_speed_predictive(i))
-
-
-
 
 diff_0_lead_time = []
 diff_3_lead_time = []
@@ -117,9 +115,6 @@ for i in range(0, len(speed_wind_predictive_true)):
         diff_72_lead_time = (calc_diff('72', diff_72_lead_time))
         diff_75_lead_time = (calc_diff('75', diff_75_lead_time))
         diff_78_lead_time = (calc_diff('78', diff_78_lead_time))
-
-
-
 
 '''
 print(diff_0_lead_time)
@@ -180,57 +175,34 @@ average_diff_75 = calc_the_average_diff_for_the_lead_time(diff_75_lead_time)
 average_diff_78 = calc_the_average_diff_for_the_lead_time(diff_78_lead_time)
 
 
-print('Средняя разность для заблаговременности 0 часов:')
-print(average_diff_0)
-print('Средняя разность для заблаговременности 3 часа:')
-print(average_diff_3)
-print('Средняя разность для заблаговременности 6 часов:')
-print(average_diff_6)
-print('Средняя разность для заблаговременности 9 часов:')
-print(average_diff_9)
-print('Средняя разность для заблаговременности 12 часов:')
-print(average_diff_12)
-print('Средняя разность для заблаговременности 15 часов:')
-print(average_diff_15)
-print('Средняя разность для заблаговременности 18 часов:')
-print(average_diff_18)
-print('Средняя разность для заблаговременности 21 час:')
-print(average_diff_21)
-print('Средняя разность для заблаговременности 24 часа:')
-print(average_diff_24)
-print('Средняя разность для заблаговременности 27 часов:')
-print(average_diff_27)
-print('Средняя разность для заблаговременности 30 часов:')
-print(average_diff_30)
-print('Средняя разность для заблаговременности 33 часа:')
-print(average_diff_33)
-print('Средняя разность для заблаговременности 36 часов:')
-print(average_diff_36)
-print('Средняя разность для заблаговременности 39 часов:')
-print(average_diff_39)
-print('Средняя разность для заблаговременности 42 часа:')
-print(average_diff_42)
-print('Средняя разность для заблаговременности 45 часов:')
-print(average_diff_45)
-print('Средняя разность для заблаговременности 48 часов:')
-print(average_diff_48)
-print('Средняя разность для заблаговременности 51 час:')
-print(average_diff_51)
-print('Средняя разность для заблаговременности 54 часа:')
-print(average_diff_54)
-print('Средняя разность для заблаговременности 57 часов:')
-print(average_diff_57)
-print('Средняя разность для заблаговременности 60 часов:')
-print(average_diff_60)
-print('Средняя разность для заблаговременности 63 часа:')
-print(average_diff_63)
-print('Средняя разность для заблаговременности 66 часов:')
-print(average_diff_66)
-print('Средняя разность для заблаговременности 69 часов:')
-print(average_diff_69)
-print('Средняя разность для заблаговременности 72 часа:')
-print(average_diff_72)
-print('Средняя разность для заблаговременности 75 часов:')
-print(average_diff_75)
-print('Средняя разность для заблаговременности 78 часов:')
-print(average_diff_78)
+def print_avrega_dif(hour: str, average_diff):
+    print('Средняя разность для заблаговременности ' + hour)
+    print(average_diff)
+
+print_avrega_dif('0 часов', average_diff_0)
+print_avrega_dif('3 часа', average_diff_3)
+print_avrega_dif('0 часов', average_diff_6)
+print_avrega_dif('0 часов', average_diff_9)
+print_avrega_dif('0 часов', average_diff_12)
+print_avrega_dif('0 часов', average_diff_15)
+print_avrega_dif('0 часов', average_diff_18)
+print_avrega_dif('0 часов', average_diff_21)
+print_avrega_dif('0 часов', average_diff_24)
+print_avrega_dif('0 часов', average_diff_27)
+print_avrega_dif('0 часов', average_diff_30)
+print_avrega_dif('0 часов', average_diff_33)
+print_avrega_dif('0 часов', average_diff_36)
+print_avrega_dif('0 часов', average_diff_39)
+print_avrega_dif('0 часов', average_diff_42)
+print_avrega_dif('0 часов', average_diff_45)
+print_avrega_dif('0 часов', average_diff_48)
+print_avrega_dif('0 часов', average_diff_51)
+print_avrega_dif('0 часов', average_diff_54)
+print_avrega_dif('0 часов', average_diff_57)
+print_avrega_dif('0 часов', average_diff_60)
+print_avrega_dif('0 часов', average_diff_63)
+print_avrega_dif('0 часов', average_diff_66)
+print_avrega_dif('0 часов', average_diff_69)
+print_avrega_dif('0 часов', average_diff_72)
+print_avrega_dif('0 часов', average_diff_75)
+print_avrega_dif('0 часов', average_diff_78)
