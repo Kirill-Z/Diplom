6e9141ad2dbba3bce0150755230fc559300533d8
Kirill-Z <zyev.99@mail.ru>
2021-05-20 01:14:57 +0700

    I haven't changed anything in this file, but it is highlighted as changed

--- building predictive and practical data.py
+++ building predictive and practical data.py
@@ -1,5 +1,5 @@
 import wind_forecast
-import practical_wind_forecast
+import practical_wind
 import plotting
 import matplotlib.pyplot as plt
 
@@ -15,14 +15,14 @@ elif value == '2':
 value = input("Observation data: If you need to calculate a point, press 1, if you need to calculate an area, "
               "press 2 or 3: ")
 if value == '1':
-    speed_wind_practical = practical_wind_forecast.main(value)
+    speed_wind_practical = practical_wind.main(value)
     plotting.plotting_wind_speed(speed_wind_practical, '%Y-%m-%d-%H:%M', 1, 'r', 'АВ-6, Наблюдение')
 elif value == '2':
-    speed_wind_practical = practical_wind_forecast.main_area(value)
+    speed_wind_practical = practical_wind.main_area(value)
     plotting.plotting_wind_speed(speed_wind_practical, '%Y-%m-%d-%H:%M', 1, 'r', 'АВ-6, Наблюдение для диапазона '
                                                                                  'времени (+-30 мин)')
 elif value == '3':
-    speed_wind_practical = practical_wind_forecast.main_area_with_every_minute(value)
+    speed_wind_practical = practical_wind.main_area_with_every_minute(value)
     plotting.plotting_wind_speed(speed_wind_practical, '%Y-%m-%d-%H:%M', 1, 'r',
                                  'АВ-6, Наблюдение для диапазона времени (+-30 мин), учитывая каждую минуту')
 
