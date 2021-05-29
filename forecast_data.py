6839aea9ed93e1864843544c25a1f007db1fbcdd
Kirill-Z <zyev.99@mail.ru>
2021-05-29 12:38:46 +0700

    fix: All bugs fixed

--- forecast_data.py
+++ forecast_data.py
@@ -2,11 +2,11 @@ import pandas as pd
 import os
 import re
 
-PATH = "/home/kirill/Downloads/Data/gfs/"
+PATH = "/home/kirill/Downloads/Data/gfs/"  # Path to files with forecast data
 
 
-# Calculation of the point number based on the specified coordinates
 def calculation_of_the_point_number():
+    """Calculation of the point number based on the specified coordinates."""
     lat = float(input('Input latitude: ') or 55.03)
     lon = float(input('Input longitude: ') or 82.6)
     point_number_in_gfc = ((lat - 43.5) * 2) * ((lon - 49.5) * 2)
@@ -14,10 +14,9 @@ def calculation_of_the_point_number():
     print(f"Point number  in gfc: {point_number_in_gfc}")
     return point_number_in_gfc
 
-    # Calculation of the area of points by the specified coordinates
-
 
 def calculation_of_the_area_of_points():
+    """Calculation of the area of points by the specified coordinates."""
     low_lat = float(input('Enter the lower value latitude: ') or 54.5)
     low_lon = float(input('Enter the lower value longitude: ') or 82.1)
     top_lat = float(input('Enter the top value latitude: ') or 55.5)
@@ -34,21 +33,22 @@ def calculation_of_the_area_of_points():
 def adding_information(data_from_file, get_data, num_item):
     correct_data = [data_from_file[num_item][0], data_from_file[num_item][1]]
     speed_data = get_data
-    if len(speed_data) > 2:
+    if len(speed_data) > 2:  # Executed if an area of points is present
         for i in range(0, len(speed_data)):
             correct_data.append(speed_data[i])
-    else:
+    else:  # Executed if point is present
         correct_data.append(speed_data)
     return correct_data
 
 
-# Obtaining data on the direction of wind speed
 def getting_need_data_for_point(point_to_calculate, data_from_file, num_item):
+    """Obtaining data on the direction of wind speed for point."""
     correct_data = data_from_file[num_item][point_to_calculate]
     return correct_data
 
 
 def getting_need_data_for_area(data_from_file, low_points_to_calculate, top_points_to_calculate, num_item):
+    """Obtaining wind speed direction data for a point area."""
     correct_data = []
     for j in range(low_points_to_calculate, top_points_to_calculate):
         correct_data.append(data_from_file[num_item][j])
@@ -56,7 +56,7 @@ def getting_need_data_for_area(data_from_file, low_points_to_calculate, top_poin
 
 
 def adding_data_to_the_speed_list(file):
-    # Adding file name, year, month, day, time
+    """Adding file name, year, month, day, time."""
     wind_data = [file, file[slice(0, 4)], file[slice(4, 6)], int(file[slice(6, 8)]), int(file[slice(11, 13)])]
     return wind_data
 
@@ -83,6 +83,7 @@ def calculation_of_the_average_speed_in_the_range(speed):
 
 
 def calculates_the_day_based_on_the_lead_time(speed_wind, reference_num):
+    """Used when the lead time exceeds a day."""
     hours_in_a_day = {1: 24, 2: 48, 3: 72, 4: 96}
     for i in hours_in_a_day:
         if hours_in_a_day[i] <= int(speed_wind[reference_num][4]) < hours_in_a_day[i + 1]:
@@ -91,6 +92,7 @@ def calculates_the_day_based_on_the_lead_time(speed_wind, reference_num):
 
 
 def calculates_month_based_on_day(speed_wind, reference_num):
+    """Used when the number of days in a month exceeds the allowable."""
     day_in_month = {1: 31, 2: (29 if (int(speed_wind[reference_num][1]) % 4 == 0) else 28), 3: 31, 4: 30, 5: 31,
                     6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
     for num_of_days in day_in_month.keys():
@@ -100,7 +102,8 @@ def calculates_month_based_on_day(speed_wind, reference_num):
             speed_wind[reference_num][2] += 1
 
 
-def calculation_of_the_year_based_on_month(speed_wind, reference_num):  # Used when the month value overflows > 13
+def calculation_of_the_year_based_on_month(speed_wind, reference_num):
+    """Used when the month value overflows > 13."""
     if int(speed_wind[reference_num][2]) > 12:
         speed_wind[reference_num][2] = speed_wind[reference_num][2] - 12
         speed_wind[reference_num][1] = int(speed_wind[reference_num][1]) + 1
@@ -116,24 +119,16 @@ def calc_for_area(data_from_file, get_data, num_item):
     return correct_data
 
 
-def write_data_to_a_file(speed, write_file, name_file):  # For convenient output in a file
-    my_file = open(PATH + name_file, 'w')
-    my_file.write('File name     |   Year |Month |  Day |Lead Time|Level|         Wind Speed\n')
-    for i in range(0, len(speed)):
-        for j in range(0, len(speed[i])):
-            if len(str(speed[i][j])) == 1:
-                write_file.write(str(speed[i][j]) + '  |   ')
-            elif len(str(speed[i][j])) == 2:
-                write_file.write(str(speed[i][j]) + ' |   ')
-            else:
-                write_file.write(str(speed[i][j]) + ' |   ')
-        write_file.write('\n')
-
-    write_file.close()
-    return print('End of writing to file')
-
-
 def write_in_file(path_to_dir, write_file, list_data):
+    """Writing the required data to a separate file.
+
+    We will use the recording of the necessary data, in this case, the name of the file
+    from which the values were taken, year, month, day, lead time, hour of calculation
+    and wind speed in a separate file, since the files with predicted data contain a large
+    amount of information and, if necessary, make calculations for these points, it will be
+    much faster to retrieve data from a file that contains only the necessary information
+
+    """
     my_file = open(path_to_dir + write_file, 'w')
     for i in range(0, len(list_data)):
         for j in range(0, len(list_data[i])):
@@ -144,15 +139,6 @@ def write_in_file(path_to_dir, write_file, list_data):
         my_file.write('\n')
     my_file.close()
     return print('End of writing list in file')
-
-
-def choice_speed_data_with_lead_time(speed_wind):
-    speed_wind_with_lead_time_on_this_day = []
-    for i in range(len(speed_wind)):
-        file_name = speed_wind[i][0][11:]
-        if int(file_name) < 24:
-            speed_wind_with_lead_time_on_this_day.append(speed_wind[i])
-    return speed_wind_with_lead_time_on_this_day
 
 
 def get_wind_speed_for_point(dirs, points_to_calculate, speed_wind, reference_num):
@@ -230,20 +216,20 @@ def get_wind_speed_for_area(dirs, low_point_number, top_point_number, speed_wind
 
 def main(value):
     reference_num = 0
-    speed_wind = []
-    speed_wind_for_forecast_data = []
+    speed_wind_training = []
+    speed_wind_test = []
     if value == '1':
         points_to_calculate = calculation_of_the_point_number() + 1
         for dirs in sorted(os.listdir(PATH)):
             if re.match('201[6,7]', dirs):
-                speed_wind = get_wind_speed_for_point(dirs, points_to_calculate, speed_wind, reference_num)
+                speed_wind_training = get_wind_speed_for_point(dirs, points_to_calculate, speed_wind_training,
+                                                               reference_num)
             elif re.match('2018', dirs):
                 reference_num = 0
-                speed_wind_for_forecast_data = get_wind_speed_for_point(dirs, points_to_calculate,
-                                                                        speed_wind_for_forecast_data, reference_num)
+                speed_wind_test = get_wind_speed_for_point(dirs, points_to_calculate, speed_wind_test, reference_num)
 
-        write_in_file(PATH, 'forecast_for_point_data', speed_wind)
-        write_in_file(PATH, 'forecast_for_point_data_', speed_wind_for_forecast_data)
+        write_in_file(PATH, 'forecast_for_point_data', speed_wind_training)
+        write_in_file(PATH, 'forecast_for_point_data_', speed_wind_test)
 
     elif value == '2':
         low_point_number, top_point_number = calculation_of_the_area_of_points()
@@ -251,14 +237,14 @@ def main(value):
         top_point_number += 1
         for dirs in sorted(os.listdir(PATH)):
             if re.match('201[6,7]', dirs):
-                speed_wind = get_wind_speed_for_area(dirs, low_point_number, top_point_number, speed_wind,
-                                                     reference_num)
+                speed_wind_training = get_wind_speed_for_area(dirs, low_point_number, top_point_number,
+                                                              speed_wind_training, reference_num)
             elif re.match('2018', dirs):
                 reference_num = 0
-                speed_wind_for_forecast_data = get_wind_speed_for_area(dirs, low_point_number, top_point_number,
-                                                                       speed_wind_for_forecast_data, reference_num)
+                speed_wind_test = get_wind_speed_for_area(dirs, low_point_number, top_point_number,
+                                                          speed_wind_test, reference_num)
 
-        write_in_file(PATH, 'forecast_for_area_data', speed_wind)
-        write_in_file(PATH, 'forecast_for_area_data_', speed_wind_for_forecast_data)
+        write_in_file(PATH, 'forecast_for_area_data', speed_wind_training)
+        write_in_file(PATH, 'forecast_for_area_data_', speed_wind_test)
 
-    return speed_wind, speed_wind_for_forecast_data
+    return speed_wind_training, speed_wind_test
